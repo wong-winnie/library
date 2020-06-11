c499ab062efca6f752d0b679e2d4f609879e7c9d
winnie <2987423164@qq.com>
2020-06-11 16:37:48 +0800

    update helptime

--- yao/helptime/helptime.go
+++ yao/helptime/helptime.go
@@ -1,6 +1,9 @@
 package helptime
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 const defaultFormat = "2006-01-02 15:04:05.000"
 
@@ -14,3 +17,9 @@ func TimeFormat(needTime, oldFormat, newFormat string) string {
 //sTime, _ := time.Parse("2006-01-02 15:04:05", thirdData.Data.StartTime)
 //eTime, _ := time.Parse("2006-01-02 15:04:05", thirdData.Data.EndTime)
 //mTime := eTime.Sub(sTime)
+
+func GetDateTimeNowInt() int64 {
+	StrDate := time.Now().Format("20060102150405")
+	DateNow, _ := strconv.ParseInt(StrDate, 10, 64)
+	return DateNow
+}
