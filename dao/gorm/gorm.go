a7192bf2f0cc9dc7d5d6975fe27378082fb7a83b
伟洪winnie <2987423164@qq.com>
2020-08-13 10:52:19 +0800

    Update gorm.go
    Update Comment

--- dao/gorm/gorm.go
+++ dao/gorm/gorm.go
@@ -17,6 +17,7 @@ func InitGorm(cfg *config.MysqlCfg) *GormMgr {
 	if err != nil {
 		log.Fatal("InitGorm Failed", err.Error())
 	}
+	//配置数偏少于MySQL配置的最大连接数(show variables like '%max_connections%')
 	conn.DB().SetMaxIdleConns(1024)
 	conn.DB().SetMaxOpenConns(1024)
 	conn.DB().SetConnMaxLifetime(9 * time.Second)
